2da6d30398c97583bb464faa944d8386c81e5728
LimitToInfinity <21stLimitless@gmail.com>
2020-10-16 13:02:50 -0600

    Adds seeds for characters, and index and show routes.

--- characters_api/db/seeds.rb
+++ characters_api/db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Character.create(
+  name: 'Luke Skywalker',
+  image: 'https://vignette.wikia.nocookie.net/star-wars-canon-extended/images/2/2c/Luke_Sky7.jpg/revision/latest/scale-to-width-down/340?cb=20180123070942',
+  likeness: 10
+)
+
+Character.create(
+  name: 'Spongebob Squarepants',
+  image: 'https://bloximages.newyork1.vip.townnews.com/kmov.com/content/tncms/assets/v3/editorial/4/c7/4c76c416-2781-51a8-a54f-5659c85408d3/5ee91aa55a4e8.image.jpg?crop=1851%2C1041%2C0%2C0&resize=1851%2C1041&order=crop%2Cresize',
+  likeness: 100
+)
+
+Character.create(
+  name: 'Dora the Explora',
+  image: 'https://s3.amazonaws.com/relaunch.noggin.com-production/wp-content/uploads/2019/06/24185057/Dora-Copy.jpg',
+  likeness: 1000
+)
